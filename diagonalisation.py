4644ee23ab45b5ee28f70fdd95a509dd4f43af8e
Branko Meeus <branko.meeus@uhasselt.be>
2025-02-20 15:32:10 +0100

    Added W(t), Weq, Peq

--- diagonalisation.py
+++ diagonalisation.py
@@ -1,17 +1,21 @@
 import numpy as np
 import numpy.linalg as lin
+import sympy as sp
+
 
 N = 4
-mu_L = 1
-mu_R = 1
+mu_L = 0
+mu_R = 0
+F = 1
+t = sp.symbols("t", real=True, positive=True)
 
-G_pL = 1/(1+np.exp(-mu_L))
+G_pL = 1/(1+sp.exp(-(mu_L + F * sp.cos(t))))
 G_mL = 1-G_pL
 
-G_pR = 1/(1+np.exp(-mu_R))
+G_pR = 1/(1+sp.exp(-mu_R))
 G_mR = 1-G_pR
 
-W = np.zeros([N,N])
+W = sp.Matrix(np.zeros([N,N]))
 
 W[1, 0] = G_pL
 W[0, 1] = G_mL
@@ -26,4 +30,18 @@ for n in range(1, N-1):
 for n in range(N):
     W[n, n] = -sum(W[:, n])
 
-l, v = lin.eig(W)
+
+Weq = W.subs({t: sp.pi/2})
+
+res = Weq.eigenvects()
+
+evals = []
+evecs = []
+
+for spc in res:
+    for degen in range(spc[1]):
+        evals.append(spc[0])
+        evecs.append(spc[2][degen])
+
+evals[-1] = 0
+
