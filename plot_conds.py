6567c293e6d0b16063fe23a9360a68deb416ba52
Branko Meeus <branko.meeus@uhasselt.be>
2025-03-24 16:52:20 +0100

    Standardized and commented plot_conds, added current finding function

--- plot_conds.py
+++ plot_conds.py
@@ -3,23 +3,63 @@ from pyplot_funcs import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-name = "loop"
-figfolder = "figs_stoch_imp"
+
+"""
+Define names and locations
+"""
+name = "4_loop"                   # Name and folder of data
+foldername = "n_sites_loop"
+
+figfolder = "figs_stoch_imp"      # destination name and folder of figure
 figname = "loop_no_inset"
-out = np.load(f"np_files/coeff_files/coeff_{name}.npz")
+
+save = False                      # Save the figure
+local = True                      # If True figure is saved in project. Otherwise, specify entire path!
+
+currents = []
+
+
+def cond(om, i, j, normal=False):
+    """
+    Calculate the conductance of the transition i --> j. This can be normalised using the conductance at zero driving.
+
+    :param om: Float/np array: The driving frequency or array of frequencies
+    :param i: Int: The site of origin of the transition
+    :param j: Int: The destination site of the transition
+    :param normal: Bool: if True returns the conductance normalised using conductance at zero frequency
+
+    :return: Float/np array: The conductance of the transition i --> j.
+    """
+    c = sum([coeffs[i, j, k] * (1 if k == 0 else (vals[k] / (1j * om - vals[k]))) for k in range(len(coeffs[0, 0, :]))])
+    if normal:
+        n = sum([coeffs[i, j, k] * (1 if k == 0 else -1) for k in range(len(coeffs[0, 0, :]))])
+        return c / n
+    else:
+        return c
+
+
+"""
+Read out data, detect transitions if necessary
+"""
+
+out = np.load(f"{foldername}/{name}.npz")
 
 vals = out["vals"]
 coeffs = out["coeffs"]
+weq = out["weq"]
+w1 = out["w1"]
+
+# Detect possible transitions from weq and w1
+if not currents:
+    for i in range(len(weq[:, 0])):
+        for j in range(i, len(weq[:, 0])):
+            if weq[i, j] != 0 or weq[j, i] != 0 or w1[i, j] != 0 or w1[j, i] != 0:
+                currents.append([i, j])
 
 
-def cond(om, i, j):
-    c = sum([coeffs[i, j, k] * (1 if k == 0 else (vals[k] / (1j * om - vals[k]))) for k in range(len(coeffs[0, 0, :]))])
-    return c
-
-def ncond(om, i, j):
-    c = sum([coeffs[i, j, k] * (1 if k == 0 else (vals[k] / (1j * om - vals[k]))) for k in range(len(coeffs[0, 0, :]))])
-    n = sum([coeffs[i, j, k] * (1 if k == 0 else -1) for k in range(len(coeffs[0, 0, :]))])
-    return c/n
+"""
+Initialise figure
+"""
 
 fig, ax = plt.subplots()
 
@@ -28,22 +68,34 @@ plt.rcParams.update({
     "font.family": "mathpazo"
 })
 
-for curr in [[0, 1], [2, 3], [0, 2]]:
-    c_i, c_j = curr
-    om_arr = np.linspace(0, 300, 10000)
-    res_arr = cond(om_arr, c_i, c_j)
+
+"""
+Plot currents
+"""
+
+# Define range of frequencies
+om_arr = np.linspace(0, 300, 10000)
+
+for curr in currents:
+    c_i, c_j = curr  # Extract transition
+    res_arr = cond(om_arr, c_i, c_j)  # calculate conductance
+    # plot result
     line = ax.plot(np.real(res_arr), np.imag(res_arr), label=f"${c_i}\\to{c_j}$")[0]
     add_arrow(line)
 
-complex_axes(ax, r"\sigma_{mn}(\omega)", x_off=[-0.015, 0.03], y_off=[0.2, 0], sz=12)
+# Add axes
+complex_axes(ax, r"\sigma_{mn}(\omega)")
+
+# Polish figure
 ax.tick_params(labelfontfamily="serif")
-
-
 ax.set_title("Periodically driven current", fontname="serif", fontsize=18)
 # ax.legend()
-ax.set_xlim([0.2, 0.65])
 plt.tight_layout()
-plt.savefig(f"C:\\Users\\lucp13819\\Pictures\\{figfolder}\\{figname}.png", dpi=600, bbox_inches='tight')
-plt.show()
 
-# TODO: Add current finding func
+if save:
+    if local:
+        plt.savefig(f"{figfolder}/{figname}.png", dpi=600, bbox_inches='tight')
+    else:
+        plt.savefig(f"C:\\Users\\lucp13819\\Pictures\\{figfolder}\\{figname}.png", dpi=600, bbox_inches='tight')
+
+plt.show()
