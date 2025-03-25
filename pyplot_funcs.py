6cc54ce391d6b5d5f6b5577307f4c39a757ba6de
Branko Meeus <branko.meeus@uhasselt.be>
2025-03-25 14:32:51 +0100

    changed heuristic for label placement

--- pyplot_funcs.py
+++ pyplot_funcs.py
@@ -57,12 +57,12 @@ def complex_axes(ax, q, x_off=None, y_off=None, sz=None, grid=True, color="k"):
 
     x_min, x_max = ax.get_xlim()        # Get ranges for heuristic placement of text
     x_range = x_max - x_min
-    if x_min < 0:
+    if x_min < 0 < x_max:
         x_min = 0
 
     y_min, y_max = ax.get_ylim()
     y_range = y_max - y_min
-    if y_min < 0:
+    if y_min < 0 < y_max:
         y_min = 0
 
     # Make y-label
@@ -71,7 +71,7 @@ def complex_axes(ax, q, x_off=None, y_off=None, sz=None, grid=True, color="k"):
             wrap=True, fontsize=sz)
 
     # Make x-label
-    ax.text(x_max - x_range / 10 + x_off[0], y_min - y_range / 15 + x_off[1],
+    ax.text(x_max - x_range / 10 + x_off[0], y_min + y_range / 50 + x_off[1],
             r"$\mathrm{Re}\, " + f"{q}$",
             wrap=True, fontsize=sz)
     return
