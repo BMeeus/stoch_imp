f327d41dfeb0622a6b5ce9293a24c592ca71f115
Branko Meeus <branko.meeus@uhasselt.be>
2025-06-10 10:54:30 +0200

    Revert "Added Symbolic and Numeric classes"
    This reverts commit a566a0deb48a1330e6b59dc7796a6cf7e044278a.

--- stoch_imp/core_classes.py
+++ stoch_imp/core_classes.py
@@ -167,21 +167,6 @@ class TrMatrix(Mat):
         return check_rates(self, verbose=verbose)
 
 
-class SymbolicMatrix:
-    def __init__(self):
-        self.is_symbolic = True
-        self.is_numeric = False
-
-
-class NumericMatrix:
-    def __init__(self):
-        self.is_symbolic = False
-        self.is_numeric = True
-        self.tol = 10**(-15)
-
-
-
-
 def arr_to_mat(arr):
     """Cast array-like object to sympy matrix and check shape requirements"""
     m = Matrix(arr)
